08a098c3250156e6d8683829967297495c00787c
kole <h.kolaric92@gmail.com>
2023-07-26 07:29:40 +0200

    fix(): add main.ts

---
+++ packages/demo/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import '../../../dist/lib/page-builder.wc.js';
+
+  let el: HTMLDivElement;
+
+  onMount(() => {
+    const instanceEl = document.createElement('jp-page-builder');
+    // const instanceEl = document.createElement('jp-bpmn-single');
+
+    el.appendChild(instanceEl);
+  })
+</script>
+
+<div bind:this={el}></div>
